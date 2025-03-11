9f7a258b0b9c6da3742f619f4df6dd85d92fdb3e
BBGaster <124209517+BBGaster@users.noreply.github.com>
2025-03-11 15:45:36 +0100

    sistemato register

--- bulkyWebBBG/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ bulkyWebBBG/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -106,6 +106,8 @@ namespace bulkyWebBBG.Areas.Identity.Pages.Account
 
             public string? Role { get; set; }
             public IEnumerable<SelectListItem> RoleList { get; set; }
+            [Required]
+            public string Name { get; set; }
             public string? StreetAdress { get; set; }
             public string? City { get; set; }
             public string? State { get; set; }
@@ -147,6 +149,12 @@ namespace bulkyWebBBG.Areas.Identity.Pages.Account
 
                 await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
                 await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
+                user.Name = Input.Name;
+                user.StreetAdress = Input.StreetAdress;
+                user.PhoneNumber = Input.PhoneNumber;
+                user.City = Input.City;
+                user.State = Input.State;
+                user.PostalCode = Input.PostalCode;
                 var result = await _userManager.CreateAsync(user, Input.Password);
 
                 if (result.Succeeded)
